cce08f4431bf21681467f1997dbcfa5b8e931d01
JUGGUM <gayoung801@naver.com>
2024-07-12 08:06:19 +0900

    Pac : 설정파일

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'App',
+/*  mounted() {
+    console.log('hello', this.hello)
+  }*/
+})
+</script>
+<!--이러한 스타일은 팀내부에 맞게 설정하면됌-->
